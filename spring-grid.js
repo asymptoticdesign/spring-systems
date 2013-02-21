7259bbdd8131f64501f41ab155adcc77d8e7185e
nathan lachenmyer <asymptoticdesign@mit.edu>
2013-02-21 01:21:07 -0500

    Finished hexagonal grid and other examples

--- spring-grid.js
+++ spring-grid.js
@@ -1,11 +1,11 @@
 //global variables
-var cell_width = 40;
-var cell_height = 40;
+var cell_width = 20;
+var cell_height = 20;
 var width = 800;
 var height = 600;
 var numParticles_X = width / cell_width;
 var numParticles_Y = height / cell_height;
-var SPRING_STRENGTH = 0.1;
+var SPRING_STRENGTH = 1.0;
 var SPRING_DAMPING = 1.5;
 var SPRING_LENGTH = 0.1;
 var ATTRACT_STRENGTH = 10000;
@@ -26,7 +26,7 @@ function setup() {
     ctx.fillStyle = "#FFFFFF";
     ctx.fillRect(0,0,width,height);
     //initialize geometry
-    initializeGrid();
+    initializeHexagons();
     //add a force
     var p = pSys.makeParticle(1, width/2, height/2, 0);
     p.makeFixed();
@@ -82,10 +82,47 @@ function initializeGrid() {
     numSprings = pSys.springs.length;
 }
 
-//function initializeLattice() {
-//}
+function initializeHexagons() {
+    //create new particle system
+    pSys = new ParticleSystem();
+    //create grid of particles
+    var particleRow = []
+    var pos_x = 0;
+    var pos_y = 0;
+    for(j = 0; j < numParticles_Y; j++) {
+	var particleRow = [];
+	for(i = 0; i < numParticles_X; i++) {
+	    var p = pSys.makeParticle(1, pos_x*0.8, pos_y*Math.sqrt(3)/2.0, 0);
+	    if (j == 0 || j == numParticles_Y - 1 || i == 0 || i == numParticles_X -1) {
+		p.makeFixed();
+	    }
+	    particleRow[particleRow.length] = p;
+	    pos_x += cell_width;
+	}	    
+	particleGrid[particleGrid.length] = particleRow;
+	pos_y += cell_height;
+	pos_x = 0;
+    }
+    numParticles = pSys.particles.length;
+    //now create grid of springs connecting them
+    for(i = 0; i < numParticles_X; i++) {
+	for(j = 0; j < numParticles_Y; j++) {
+	    if (i < numParticles_X - 1 && j < numParticles_Y - 1) {
+		pSys.makeSpring(particleGrid[j][i], particleGrid[j][i+1], SPRING_STRENGTH, SPRING_DAMPING, SPRING_LENGTH);
+	    }		
+	    if (j < numParticles_Y - 1) {
+		if (i % 2 == 0 && j % 2 == 1) {
+		    pSys.makeSpring(particleGrid[j][i], particleGrid[j+1][i], SPRING_STRENGTH, SPRING_DAMPING, SPRING_LENGTH);
+		}
+		if (i % 2 == 1 && j % 2 == 0) {
+		    pSys.makeSpring(particleGrid[j][i], particleGrid[j+1][i], SPRING_STRENGTH, SPRING_DAMPING, SPRING_LENGTH);
+		}
 
-//function intiializeRad
+	    }
+	}
+    }
+    numSprings = pSys.springs.length;
+}
 
 function render() {
 //    ctx.strokeStyle="#000000";
